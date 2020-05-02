3e01ddfcd04f97a9b1b4dcfbfb52c7a6add47d6f
przemek-bykowski <przemek@bykowski.pl>
2020-05-02 02:34:43 +0200

    daw

--- server.js
+++ server.js
@@ -1,26 +1,10 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 
+app.use(express.static(__dirname + '/dist/angular-heroku'));
 
-// Run the app by serving the static files in the dist directory
-app.use(express.static(__dirname + '/dist'));
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-// });
-
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-}
-
-app.get('*', (request, response) => {
-  response.sendfile('./src/index.html')
+app.post('/*', function(req, res){
+  res.sendFile(__dirname +  '/dist/angular-heroku/index.html');
 });
-
-
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4200);
+app.listen(4200);
 
